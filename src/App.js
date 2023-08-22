441851f61cc6c54fa4bfb49de786994ca0eca3c1
Madhur Gupta <madhurmawai@gmail.com>
2023-08-22 12:37:21 +0530

    feat: Banner

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import Body from "./Components/Body/Body";
 import Head from "./Components/Header/Head";
 
 function App() {
   return (
     <div>
       <Head />
+      <Body/>
     </div>
   );
 }
---
+++ src/Components/Body/Body.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import BodyBanner from './BodyBanner'
+import CategoryWidget from './CategoryWidget'
+
+const Body = () => {
+  return (
+    <div>
+      <BodyBanner/>
+      <CategoryWidget/>
+    </div>
+  )
+}
+
+export default Body
