49a0a500b6863b36f3d4827f4395229d0e7490bc
Madhur Gupta <madhurmawai@gmail.com>
2023-08-28 10:38:05 +0530

    feat: mobile section

--- src/Components/LandingPage/LandingPage.js
+++ src/Components/LandingPage/LandingPage.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import SearchLocation from './SearchLocation'
 import LandingPgInfo from './LandingPgInfo'
+import MobileAd from './MobileAd'
 
 const LandingPage = () => {
   return (
     <div>
       <SearchLocation/>
       <LandingPgInfo/>
+      <MobileAd/>
       
     </div>
   )
---
+++ src/Components/LandingPage/MobileAd.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+const MobileAd = () => {
+  return (
+    <div>
+      <div className="grid grid-flow-col">
+        <div className="col-span-5">
+          <div className="grid grid-rows-3 grid-flow-row-dense">
+            
+            <div className="row-span-1">
+              <h1>ABC</h1>
+            </div>
+
+            <div className="row-span-1">
+              <h1>ABC</h1>
+            </div>
+
+            <div className="row-span-1">
+              <h1>ABC</h1>
+            </div>
+
+          </div>
+        </div>
+        <div className="col-span-7"></div>
+      </div>
+    </div>
+  );
+};
+
+export default MobileAd;
